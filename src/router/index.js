909d712aab318426392e61ebcb213bd4e288e902
Balduino Fernando <bafernando94@gmail.com>
2023-04-16 21:27:34 +0100

    UI for Login and Register Views Done

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       name: 'Login',
       component: () => import('../views/Login.vue')
     },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('../views/Register.vue')
+    },
   ]
 })
 
